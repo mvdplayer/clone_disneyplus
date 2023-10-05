dcca00f880e292493a399710aa2bb617fb983fc3
Marcos Vinicius <fernandes.mvd@gmail.com>
2023-10-04 22:28:58 -0300

    ajudets no mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const aituraHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < aituraHero ) {
+            ocultaElementoNoHeader();
+        } else {
+            exibeElementosNoHeader();
+        }
+    }) 
     
 
     for (let i = 0; i < buttons.length; i++) {
@@ -12,7 +26,28 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function ocultaElementoNoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosNoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elemntoPai = elemento.target.parentNode;
+
+    elemntoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
